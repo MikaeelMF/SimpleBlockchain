4b65e9a19e100ea2d023cca86986bdeb7ca6056e
Mikaeel Mayeli <mikaeelmayeli@gmail.com>
2022-03-06 18:00:48 +0330

    use constant defined values for db address and buckets

--- blockchain.go
+++ blockchain.go
@@ -11,6 +11,9 @@ import (
 // Genesis Block Hash: c4b64ddbaff431c9bf1e5b35752d8960507336098a2c612fd1374dda944e9e8b04fbefda96a7153ff82ab8b350ebfa9c66696c3a47196fbdc44ed1ce13b89f2f
 // Genesis Block Hash in Bytes: [196 182 77 219 175 244 49 201 191 30 91 53 117 45 137 96 80 115 54 9 138 44 97 47 209 55 77 218 148 78 158 139 4 251 239 218 150 167 21 63 248 42 184 179 80 235 250 156 102 105 108 58 71 25 111 189 196 78 209 206 19 184 159 47]
 
+const databaseAddress = "./dbfile"
+const blocksBucket = "blocksBucket"
+
 type Blockchain struct {
 	head []byte
 	db   *bolt.DB
@@ -22,7 +25,7 @@ func (bc *Blockchain) AddBlock(data string) {
 
 	// Get the last block
 	_ = bc.db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("blocksBucket"))
+		b := tx.Bucket([]byte(blocksBucket))
 		tail = b.Get([]byte("t"))
 		tailBlock = Deserialize(b.Get(tail))
 		return nil
@@ -33,7 +36,7 @@ func (bc *Blockchain) AddBlock(data string) {
 
 	// Add the new block to database and make the tail point to the last block
 	_ = bc.db.Update(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("blocksBucket"))
+		b := tx.Bucket([]byte(blocksBucket))
 		b.Put(newBlock.Hash[:], Serialize(newBlock))
 		b.Put([]byte("t"), newBlock.Hash[:])
 		return nil
@@ -42,14 +45,14 @@ func (bc *Blockchain) AddBlock(data string) {
 
 func NewBlockchain() *Blockchain {
 	var head []byte
-	db, _ := bolt.Open("./dbfile", 0600, nil)
+	db, _ := bolt.Open(databaseAddress, 0600, nil)
 	_ = db.Update(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("blocksBucket"))
+		b := tx.Bucket([]byte(blocksBucket))
 		if b == nil {
 			genesisBlock := &Block{blockHeight: 0, Data: []byte("Genesis Block"), prevBlockHash: [sha512.Size]byte{}, nonce: 0}
 			genesisBlock.Hash = sha512.Sum512(bytes.Join([][]byte{genesisBlock.prevBlockHash[:], genesisBlock.Data, []byte(strconv.FormatUint(genesisBlock.blockHeight, 10))}, []byte{}))
 
-			b, _ := tx.CreateBucket([]byte("blocksBucket"))
+			b, _ := tx.CreateBucket([]byte(blocksBucket))
 			_ = b.Put(genesisBlock.Hash[:], Serialize(genesisBlock))
 			_ = b.Put([]byte("t"), genesisBlock.Hash[:])
 			head = genesisBlock.Hash[:]
