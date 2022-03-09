dfd5887e501810497a09687b7a9c4f584136308d
Mikaeel Mayeli <mikaeelmayeli@gmail.com>
2022-03-09 18:04:31 +0330

    proofOfWork.go has no bug!

--- proofOfWork.go
+++ proofOfWork.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"crypto/sha512"
 	"math/big"
 )
 
@@ -10,6 +9,6 @@ const targetBits = 24
 // Returnes target for proof of work
 func GetTarget() *big.Int {
 	target := big.NewInt(1)
-	target.Lsh(target, uint(sha512.Size-targetBits))
+	target.Lsh(target, uint(512-targetBits))
 	return target
 }
