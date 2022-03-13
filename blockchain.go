3f99cf55cc03a70522de4f70dff238fd59888f48
Mikaeel Mayeli <mikaeelmayeli@gmail.com>
2022-03-13 13:47:02 +0330

    Rework blockchian.go v1.1, now using leveldb with no bugs

--- blockchain.go
+++ blockchain.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	bolt "go.etcd.io/bbolt"
+	"github.com/syndtr/goleveldb/leveldb"
 )
 
-const databaseAddress = "./dbfile"
-const blocksBucket = "blocksBucket"
+const databaseAddress = "./database"
 
 type Blockchain struct {
-	db       *bolt.DB
+	db       *leveldb.DB
 	iterator *blockchainIterator
 }
 
@@ -17,80 +16,67 @@ type blockchainIterator struct {
 }
 
 // Generates a new blockchain with a genesis block and returns the result
-func NewBlockchain() *Blockchain {
-	db, _ := bolt.Open(databaseAddress, 0600, nil)
-	_ = db.Update(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(blocksBucket))
-		if b == nil {
-			b, _ := tx.CreateBucket([]byte(blocksBucket))
-			genesisBlock := generateGenesisBlock()
-			genesisBlockHash := genesisBlock.GetBlockHash()
-			b.Put(genesisBlockHash[:], genesisBlock.BlockEncoder())
-			b.Put([]byte("t"), genesisBlockHash[:])
-		}
-		return nil
-	})
-	return &Blockchain{db: db, iterator: &blockchainIterator{visited: nil}}
+func InitBlockchain() *Blockchain {
+	ldb, _ := leveldb.OpenFile(databaseAddress, nil)
+	_, err := ldb.Get([]byte("t"), nil)
+	if err != nil {
+		gb := genesisBlockGenerator()
+		gbHash := gb.GetBlockHash()
+		ldb.Put([]byte("t"), gbHash[:], nil)
+		ldb.Put(gbHash[:], gb.BlockEncoder(), nil)
+	}
+	return &Blockchain{db: ldb, iterator: &blockchainIterator{nil}}
 }
 
-// AddBlock function appends a new block to current blockchain with given data
-func (bc *Blockchain) AddBlock(data string) {
+// Generates the genesis Block and returnes the result
+func genesisBlockGenerator() *Block {
+	return NewBlock("Genesis Block", nil)
+}
 
+func (bc *Blockchain) AddBlock(data string) {
 	// This function consists of three parts:
 	// 1. Prepare by getting the last mined block in blockchain
 	// 2. Generate a new block using block.go
 	// 3. Append the new block
 
 	//1. Preparation:
-	var hashLastBlock []byte // hash of the last block in blockchain retrived from database using "t" key in []byte format
-	var lastBlock *Block     // last block retrived from the database using its hash as a key
+	var hashLastBlock []byte    // hash of the last block in blockchain retrived from database using "t" key in []byte format
+	var lastBlockEncoded []byte // last block in encoded format retrived from the database using its hash as a key
+	var lastBlock *Block        // last block decoded
 
-	bc.db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(blocksBucket))
-		hashLastBlock = b.Get([]byte("t"))
-		lastBlock = BlockDecoder(b.Get(hashLastBlock))
-		return nil
-	})
+	hashLastBlock, _ = bc.db.Get([]byte("t"), nil)
+	lastBlockEncoded, _ = bc.db.Get(hashLastBlock, nil)
+	lastBlock = BlockDecoder(lastBlockEncoded)
 
 	// 2. Generation of New Block
 	newBlock := NewBlock(data, lastBlock)
 
 	// 3. Append the new block to the blockchain
-	bc.db.Update(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(blocksBucket))
-		newBlockHash := newBlock.GetBlockHash()
-		b.Put(newBlockHash[:], newBlock.BlockEncoder()) // Append the new block to database
-		b.Put([]byte("t"), newBlockHash[:])             // Update the tail for database
-		return nil
-	})
-}
-
-// Generates the genesis Block and returnes the result
-func generateGenesisBlock() *Block {
-	genesisBlock := NewBlock("Genesis Block", nil)
-	return genesisBlock
+	newBlockHash := newBlock.GetBlockHash()
+	bc.db.Put(newBlockHash[:], newBlock.BlockEncoder(), nil)
+	bc.db.Put([]byte("t"), newBlockHash[:], nil)
 }
 
 // Returns the previous block of the last visited block
 // If no block has been visited yet, it will start from the latest mined block
+
 func (bc *Blockchain) GetPreviousBlock() *Block {
+	var prevBlockHash []byte
 	var prevBlock *Block
 	iter := bc.iterator
+
 	if iter.visited == nil {
-		bc.db.View(func(tx *bolt.Tx) error {
-			b := tx.Bucket([]byte(blocksBucket))
-			prevBlock = BlockDecoder(b.Get([]byte("t")))
-			bc.iterator.visited = prevBlock
-			return nil
-		})
+		prevBlockHash, _ = bc.db.Get([]byte("t"), nil)
+		if prevBlockHash == nil {
+			panic("No Block in the Blockchain. Check your Initialization")
+		}
 	} else {
-		bc.db.View(func(tx *bolt.Tx) error {
-			b := tx.Bucket([]byte(blocksBucket))
-			prevBlockHash := iter.visited.GetPreviousBlockHash()
-			prevBlock = BlockDecoder(b.Get(prevBlockHash[:]))
-			bc.iterator.visited = prevBlock
-			return nil
-		})
+		prevBlockHashTemp := iter.visited.GetPreviousBlockHash()
+		prevBlockHash = prevBlockHashTemp[:]
 	}
+	prevBlockEncoded, _ := bc.db.Get(prevBlockHash[:], nil)
+	prevBlock = BlockDecoder(prevBlockEncoded)
+	bc.iterator.visited = prevBlock
+
 	return prevBlock
 }
